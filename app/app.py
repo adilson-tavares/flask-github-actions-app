cd1349474c8d1e67c85000ba0b4347ed7a3455fb
adilson-tavares <tavares.adilson.c@gmail.com>
2024-07-07 21:34:58 -0400

    feat: Atualiza python app

--- app/app.py
+++ app/app.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-if __name__=="__main__":
-    app.run(debug=True,host="0.0.0.0",port=9001)
+@app.route('/about')
+def about():
+    return 'About'
